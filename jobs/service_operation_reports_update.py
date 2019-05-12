c8cc68fc7cf12104ec68d927d923919eca38c496
Francisco Carpio <carpiofj@gmail.com>
2019-05-12 11:35:11 +0200

    modified collection name

--- jobs/service_operation_reports_update.py
+++ jobs/service_operation_reports_update.py
@@ -6,7 +6,8 @@ def run(cimi_api_url="http://cimi:8201/api", last_run=None):
     resource_name = "service-operation-report"
     query = '$orderby=created:desc&$filter=created>"%s"&$last=100&$filter=content/state="UPDATED"' % last_run
     collection = cl.local_get(resource_name, query=query)
-    events = collection[resource_name]
+    collection_name = "serviceOperationReports"
+    events = collection[collection_name]
     if events:
         last_run = events[0]["created"]
     print(events)
