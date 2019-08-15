b6efa5137be4e52e7541ad1042a1a2ec9cc9b4cb
Francisco Carpio <carpiofj@gmail.com>
2019-08-15 13:51:06 +0200

    Fixes queries in service-operation-reports jobs

--- jobs/service_operation_reports_update.py
+++ jobs/service_operation_reports_update.py
@@ -1,14 +1,23 @@
 import json
 from utils.CIMIClient import CIMIClient
 
+
 def run(cimi_api_url="http://cimi:8201/api", last_run=None):
     cl = CIMIClient(cimi_api_url)
     resource_name = "service-operation-report"
-    query = '$orderby=created:desc&$filter=created>"%s"&$last=100&$filter=content/state="UPDATED"' % last_run
-    collection = cl.local_get(resource_name, query=query)
     collection_name = "serviceOperationReports"
-    events = collection[collection_name]
-    if events:
-        last_run = events[0]["created"]
+    query = '$orderby=created:desc&$filter=updated>"%s"&$last=100' % last_run
+    events = []
+    try:
+        collection = cl.local_get(resource_name, query=query)
+        events = collection[collection_name]
+        if events:
+            last_run = events[0]["updated"]
+    except KeyError:
+        print("{} doesn't exist in CIMI output".format(collection_name))
+    except IndexError:
+        print("No resources were found in CIMI")
+    except:
+        print("Unknown error")
     print(events)
     return events, last_run
